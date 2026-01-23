1b9f6282719311896132d918f0d9895460e91fcf
FARSEEN <farseenahammad6@gamil.in>
2026-01-23 11:07:54 +0530

    new

--- api/index.py
+++ api/index.py
@@ -1,9 +1,12 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "firstproject.settings"
 )
+
+django.setup()
 
 application = get_wsgi_application()
