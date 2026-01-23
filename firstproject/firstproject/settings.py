909cb20ef82d79213f8366b3f91556137582cf8d
FARSEEN <farseenahammad6@gamil.in>
2026-01-23 11:05:07 +0530

    new

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -35,10 +35,10 @@ RAZORPAY_KEY_SECRET="Y6iC3HAclGLT4qUHgXtme5Bz"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Allow all hosts (use with caution in production)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
