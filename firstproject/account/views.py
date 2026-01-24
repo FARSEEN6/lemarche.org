2af93c6daae83523db606db4ff4b7691826e90ed
ilyasvengara <muhammedilyaschalil@gmail.com>
2026-01-24 12:23:10 +0530

    first commit

--- firstproject/account/views.py
+++ firstproject/account/views.py
@@ -87,7 +87,8 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect("cart")
+    messages.success(request, "Added to cart")
+    return redirect(request.META.get("HTTP_REFERER", "product"))
 
 
 @login_required
